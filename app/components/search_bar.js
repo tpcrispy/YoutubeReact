73e912899424b64c116bdf918507bb85b89b0cf8
Thomas Smith <thomas.1993.smith@gmail.com>
2016-06-07 15:03:34 +1000

    added bootstrap and wired up YTsearch

--- app/components/search_bar.js
+++ app/components/search_bar.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>
-      <input />
-    </div>
-  );
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Type here' };
+  }
+
+  render(){
+    return (
+      <div>
+        <input
+        value={this.state.term}
+        onChange = {event => this.setState({term: event.target.value })} />
+      </div>
+    );
+  }
+}
+
 
 export default SearchBar;
