9f85620c3d10e8bafe7b81e7dbf160b8d51eac60
Thomas Smith <thomas.1993.smith@gmail.com>
2016-06-08 11:45:42 +1000

    fixed searchbar css - was targeting wrong element

--- app/components/search_bar.js
+++ app/components/search_bar.js
@@ -9,8 +9,8 @@ class SearchBar extends Component {
 
   render(){
     return (
-      <div>
-        <input className="search-bar"
+      <div className="search-bar">
+        <input
         value={this.state.term}
         onChange = {event => this.setState({term: event.target.value })} />
       </div>
